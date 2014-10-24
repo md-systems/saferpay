a6a93abd5c40434f6b44cf6447d65bc3068220ab
ChrisjuhB <christian.broekmeulen@hotmail.com>
2014-10-24 12:21:55 +0200

    Saferpay Test form created

---
+++ saferpay_test/src/Form/SaferpayTestForm.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\payment_saferpay_test\Form\SaferpayTestForm.
+ */
+
+namespace Drupal\payment_saferpay_test\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\taxonomy\TermInterface;
+
+/**
+ * Builds the form to delete a forum term.
+ */
+class SaferpayTestForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'saferpay_test_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, TermInterface $taxonomy_term = NULL) {
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Test It!'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    drupal_set_message("Form submitted");
+  }
+
+}
